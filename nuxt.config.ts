544e121ff60e35e77f8ba30bdd6e2287876e2a1f
sowousmane <sowousmane4811@gmail..com>
2025-04-22 23:18:39 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   ssr: false,
-  target: 'static',
+  nitro: {
+    preset: 'static'
+  },
   app: {
     buildAssetsDir: 'assets',
+    head: {
+      title: 'SoowCode - Autonomiser les femmes dans la tech',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { 
+          name: 'description', 
+          content: 'SoowCode est une plateforme éducative dédiée à l\'autonomisation des femmes dans le domaine de l\'informatique.'
+        }
+      ]
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
@@ -16,20 +30,6 @@ export default defineNuxtConfig({
     classSuffix: '',
     preference: 'system',
     fallback: 'light'
-  },
-  app: {
-    head: {
-      title: 'SoowCode - Autonomiser les femmes dans la tech',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { 
-          hid: 'description', 
-          name: 'description', 
-          content: 'SoowCode est une plateforme éducative dédiée à l\'autonomisation des femmes dans le domaine de l\'informatique.'
-        }
-      ]
-    }
   },
   googleFonts: {
     families: {
